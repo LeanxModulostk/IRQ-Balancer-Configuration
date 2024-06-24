778a945e0ae8e24ab9e83ebe61f0cf4779f44441
@LeanHijosdesusMadres <105339980+LeanxModulostk@users.noreply.github.com>
2024-06-24 04:27:51 -0500

    Delete common directory

--- common/install.sh
+++
@@ -1,4 +0,0 @@
-bash /data/adb/modules_update/irqbalance/service2.sh
-rm /data/adb/modules_update/irqbalance/service2.sh
-
-nohup am start -a android.intent.action.VIEW -d https://t.me/modulostk >/dev/null 2>&1 &
